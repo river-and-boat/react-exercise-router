b91ff6b1d00f825ba230d76c490510121bd76cdc
RiverAndBoat <842714673@qq.com>
2020-08-12 21:38:23 +0800

    finish exercise-4

--- src/exercise-4/components/User.js
+++ src/exercise-4/components/User.js
@@ -1,11 +1,18 @@
 import React from 'react';
 
-const User = () => {
-  return (
-    <div>
-      User profile page.
-    </div>
-  );
+const User = (props) => {
+    const input = props.match.params.user;
+    const num = Number(input);
+    if (!isNaN(num) || input == "about") {
+        return (
+            <div>
+                User profile page.
+            </div>
+        );
+    } else {
+        props.history.push('/error/nonNumber');
+        return '';
+    }
 };
 
 export default User;
